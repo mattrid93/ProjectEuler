c10463e60521befb85a7fc4f2776aafdfd626a03
Matt Ridley <matthew.ridley@physics.ox.ac.uk>
2017-02-16 08:36:27 +0000

    Problem 7

---
+++ probs/prob7.py
@@ -0,0 +1,25 @@
+"""Problem 7: 100001st prime.
+
+Brute force again"""
+import unittest
+from utils.primes import is_prime
+
+def nth_prime(n):
+    """Returns nth prime"""
+    current = 1
+    primes = 0
+    while primes < n:
+        current += 1
+        if is_prime(current):
+            primes += 1
+    return current
+
+
+class TestMultiples(unittest.TestCase):
+    def test_nth_prime(self):
+        self.assertEqual(nth_prime(1), 2)
+        self.assertEqual(nth_prime(6), 13)
+
+if __name__ == "__main__":
+    print(nth_prime(10001))
+    unittest.main()
