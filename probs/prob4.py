a7db7c3b4f2628084125da916c1a63b3ae6e6b98
Matt Ridley <matthew.ridley@physics.ox.ac.uk>
2017-02-16 00:40:12 +0000

    Problem 4

---
+++ probs/prob4.py
@@ -0,0 +1,31 @@
+"""Problem 4: Largest palindrome product"""
+import unittest
+
+def is_palindrome(n):
+    """Tests if n is a palindrome"""
+    return str(n) == str(n)[::-1]
+
+def largest_palindrome_product(d):
+    """Finds the largest palindrome product of 2 numbers with d digits"""
+    lim = int("1"+"0"*d)
+    largest = 0
+    for i in range(lim):
+        for j in range(lim):
+            if is_palindrome(i*j) and i*j > largest:
+                largest = i*j
+    return largest
+
+class TestMultiples(unittest.TestCase):
+    def test_is_palindrome(self):
+        self.assertEqual(is_palindrome(0), True)
+        self.assertEqual(is_palindrome(7), True)
+        self.assertEqual(is_palindrome(15), False)
+        self.assertEqual(is_palindrome(192), False)
+        self.assertEqual(is_palindrome(14241), True)
+
+    def test_largest_palindrom_product(self):
+        self.assertEqual(largest_palindrome_product(2), 9009)
+
+if __name__ == "__main__":
+    print(largest_palindrome_product(3))
+    unittest.main()
