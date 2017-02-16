1dd11d4f8135fb3320eda6e97f55d180bb3d72f7
Matt Ridley <matthew.ridley@physics.ox.ac.uk>
2017-02-16 22:29:39 +0000

    Finished Problem 18

--- probs/prob18.py
+++ probs/prob18.py
@@ -3,91 +3,40 @@
 Dijsktra's algorithm"""
 import unittest
 
-class Node:
-    def __init__(self, weight):
-        self.weight = weight
-        self.children = []
-        self.dist = None
-        self.prev = None
-
-    def add_child(self, child):
-        self.children.append(child)
-
-class Tree:
-    def __init__(self, headnode):
-        self.headnode = headnode
-
-    def calc_max_path_sum(self):
-        
-
-    def _calc_max_path_sum(self):
-        # depth first search to generate dicts
-        S, V = [], []
-        dist = {}
-        prev = {}
-        S.insert(0, self.headnode)
-        while S != []:
-            v = S.pop(0)
-            if v not in V:
-                v.dist = 0
-                V.append(v)
-                for c in v.children:
-                    S.insert(0, c)
-        # Dijsktra's algorithm
-        source = Node(0)
-        source.children.append(self.headnode)
-        source.dist = 0
-        V.insert(0, source)
-        while V:
-            print([n.dist for n in V])
-            u = V.pop(0)
-            if not u.children:
-                max_sum = 0
-                while u.prev:
-                    u = u.prev
-                    max_sum += u.weight
-                return -max_sum
-            for v in u.children:
-                if v in V:
-                    alt = u.dist + v.weight
-                    if alt < v.dist:
-                        v.dist = alt
-                        v.prev = u
-            V.sort(key=lambda n: n.dist)
-        return False
-
-
-
-def tree_parser(filename):
-    """Parses file and returns connected tree"""
+def file_reader(filename):
     with open(filename) as f:
-        hn_weight = int(f.readline())
-        headnode = Node(hn_weight)
-        tree = Tree(headnode)
-        upper_layer = [headnode]
+        tree = []
         for line in f.readlines():
-            weights = [-int(x) for x in line.split()]
-            current_layer = [Node(x) for x in weights]
-            for i, node in enumerate(current_layer[:-1]):
-                upper_layer[i].add_child(node)
-            for i, node in enumerate(current_layer[1:]):
-                upper_layer[i].add_child(node)
-            upper_layer = current_layer
-        target = Node(0)
-        target.dist = 0
-        for n in upper_layer:
-            n.children.append(target)
+            weights = [int(x) for x in line.split()]
+            tree.append(weights)
         return tree
 
+def path_finder(tree):
+    paths = []
+    for row in tree:
+        paths.append([0]*len(row))
+    paths[0][0] = tree[0][0]
+    for i in range(1, len(tree)):
+        for j in range(len(tree[i])-1):
+            paths[i][j] = tree[i][j] + paths[i-1][j]
+        for j in range(1, len(tree[i])):
+            paths[i][j] = max(paths[i][j], tree[i][j] + paths[i-1][j-1])
+    return max(paths[-1])
 
 class TestFunctions(unittest.TestCase):
-    def test_max_path(self):
-        tree = tree_parser("inputs/prob18_test.txt")
-        self.assertEqual(tree.calc_max_path_sum(), 23)
-        tree = tree_parser("inputs/prob18_test2.txt")
-        self.assertEqual(tree.calc_max_path_sum(), 308)
+    def test_file_reader(self):
+        self.assertEqual(file_reader("inputs/prob18_test.txt"), [[3], [7, 4],
+                                                                 [2, 4, 6],
+                                                                 [8, 5, 9, 3]])
+
+    def test_path_finder(self):
+        tree = file_reader("inputs/prob18_test.txt")
+        self.assertEqual(path_finder(tree), 23)
+        tree = file_reader("inputs/prob18_test2.txt")
+        self.assertEqual(path_finder(tree), 308)
+
 
 if __name__ == "__main__":
-    tree = tree_parser("inputs/prob18.txt")
-    print(tree.calc_max_path_sum())
+    tree = file_reader("inputs/prob18.txt")
+    print(path_finder(tree))
     unittest.main()
